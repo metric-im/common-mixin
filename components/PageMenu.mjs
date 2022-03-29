1834076be1a51d2c63930322ad0e7e4d3ebac2dc
Michael Sprague <michael@sprague.com>
2022-03-29 13:45:22 -0400

    customized menu

--- components/PageMenu.mjs
+++ components/PageMenu.mjs
@@ -7,21 +7,12 @@ export default class PageMenu extends Component {
     async render(element) {
         await super.render(element);
         let tray = this.div("nav-tray");
-        for (let item of this.menu) {
+        for (let item of (this.props.menu||[])) {
             let btn = this.div('nav-button');
             btn.innerHTML = `<span class="icon icon-${item.icon}"></span><span class="text">${item.title}</span>`;
             btn.addEventListener('click',()=>{
                 document.location.href = item.target;
             })
         }
-    }
-    get menu() {
-        return [
-            {title:'Status',target:'/#Home',icon:'home'},
-            {title:'Explore',target:'/#Explore',icon:'ruler'},
-            {title:'Ontology',target:'/#Ontology',icon:'rainbow'},
-            {title:'Configure',target:'/#Configure',icon:'tools'},
-            {title:'Docs',target:'/#Wiki',icon:'open-book'}
-        ]
     }
 }
