0a2af9bcf293d895705358a2a10e54c7014ced73
Michael Sprague <michael@sprague.com>
2022-05-04 22:15:16 -0400

    buttons

--- components/Button.mjs
+++ components/Button.mjs
@@ -7,7 +7,8 @@ export class Button extends Component {
     async render(element) {
         await super.render(element);
         this.button = document.createElement('button');
-        this.button.innerHTML = this.props.title;
+        this.button.innerHTML = `<span class="icon icon-${this.props.icon||"circle"}"></span>`;
+        if (this.props.title) this.button.innerHTML += ` <span>${this.props.title}</span>`;
         this.button.addEventListener('click',this.props.onClick);
         this.element.append(this.button);
     }
