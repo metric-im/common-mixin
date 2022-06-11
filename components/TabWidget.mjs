2895aba490bf2a56e7f73a52508e5462f9acff8f
Michael Sprague <michael@sprague.com>
2022-06-11 11:48:50 -0400

    update tabwidget with component

--- components/TabWidget.mjs
+++ components/TabWidget.mjs
@@ -14,16 +14,42 @@ export default class TabWidget extends Component {
             tab.innerHTML = block.title;
             tab.setAttribute('index',index++);
             tab.addEventListener('click',this.setActive.bind(this));
+            await block.component.render(this.tabContent);
+            block.component.element.classList.add('contentComponent');
         }
         this.select(this.props.selected || 0);
     }
     setActive(e) {
-        this.select(e.target.getAttribute('index'));
+        this.select(parseInt(e.target.getAttribute('index')));
     }
     select(index) {
         this.tabSet.querySelectorAll('.selected').forEach(item=>item.classList.remove('selected'));
-        this.tabContent.innerHTML="";
-        this.tabContent.appendChild(this.props.tabs[index].element);
-        this.tabSet.children[index].classList.add('selected');
+        this.tabContent.querySelectorAll('.selected').forEach(item=>item.classList.remove('selected'));
+        let tab;
+        if (typeof index === 'undefined') {
+            tab = this.tabSet.querySelector(`:not(.hidden)`);
+        } else {
+            tab = this.tabSet.children[index];
+            if (tab && tab.classList.contains('hidden')) tab = null;
+        }
+        if (tab) {
+            tab.classList.add('selected');
+            this.tabContent.children[index].classList.add('selected');
+        }
+    }
+
+    hideTab() {
+        let tabNames = Array.from(arguments);
+        for (let tab of this.tabSet.children) {
+            if (tabNames.includes(tab.innerHTML)) tab.classList.add('hidden');
+        }
+        this.select(0);
+    }
+    showTab() {
+        let tabNames = Array.from(arguments);
+        for (let tab of this.tabSet.children) {
+            if (tabNames.includes(tab.innerHTML)) tab.classList.remove('hidden');
+        }
+        this.select(0);
     }
 }
