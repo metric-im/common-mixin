f2d5410cb82ee9b952cd19b2c41902a15fcb6ca6
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-12-02 16:59:57 +0600

    new component loader.mjs and etc

--- components/Button.mjs
+++ components/Button.mjs
@@ -10,6 +10,7 @@ export class Button extends Component {
         this.button.innerHTML = `<span class="icon icon-${this.props.icon||"circle"}"></span>`;
         if (this.props.title) this.button.innerHTML += ` <span>${this.props.title}</span>`;
         this.button.addEventListener('click',this.props.onClick);
+        this.button.disabled = this.props.disabled ?? false
         this.element.append(this.button);
     }
 }
