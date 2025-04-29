dea1680dd768b02ec2f445c46b77c29220f4a823
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2025-04-29 20:06:58 +0600

    59-link-status-icon-for-activity

--- components/Button.mjs
+++ components/Button.mjs
@@ -14,4 +14,15 @@ export class Button extends Component {
         this.element.append(this.button);
         if (this.props.layout) this.element.classList.add(this.props.layout);
     }
+
+    loading() {
+        this.button.innerHTML = '<img src="assets/loader.gif" style="width: .9rem; height: .9rem">'
+        this.button.disabled = true
+    }
+
+    stopLoading() {
+        this.button.innerHTML = `<span class="icon icon-${this.props.icon||"circle"}"></span>`;
+        if (this.props.title) this.button.innerHTML += ` <span>${this.props.title}</span>`;
+        this.button.disabled = this.props.disabled ?? false
+    }
 }
